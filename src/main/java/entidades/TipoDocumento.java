036392f71dced4c26d2e44ec3ef7e4eaee63f724
jr256 <joser.72@outlook.com>
2022-10-28 19:48:49 -0500

    .

--- src/main/java/entidades/TipoDocumento.java
+++ src/main/java/entidades/TipoDocumento.java
@@ -4,8 +4,7 @@ public class TipoDocumento {
 	private String id;
 	private String documento;
 	
-	
-	public String getId() {
+		public String getId() {
 		return id;
 	}
 	public void setId(String id) {
